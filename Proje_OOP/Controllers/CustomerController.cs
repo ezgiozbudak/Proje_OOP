f4e206fe2b9322ae8e9d102fd623c64f81047cb6
ezgiozbudak <ezgizbudak@outlook.com>
2024-07-01 15:04:17 +0300

    ...

---
+++ Proje_OOP/Controllers/CustomerController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.Entities;
+using Proje_OOP.ProjectContext;
+
+namespace Proje_OOP.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = context.Customers.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            if (customer.Name.Length >= 6 && customer.City != "" && customer.City.Length >= 3)
+            {
+                context.Add(customer);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.message = "Hatalı Kullanım";
+                return View();
+            }
+        }
+
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value=context.Customers.Where(x=>x.Id==id).FirstOrDefault(); 
+            context.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            var value = context.Customers.Where(x => x.Id == id).FirstOrDefault();
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            if (customer.Name.Length >= 6 && customer.City != "" && customer.City.Length >= 3)
+            {
+                var value = context.Customers.Where(x => x.Id == customer.Id).FirstOrDefault();
+                value.City = customer.City;
+                value.Name = customer.Name;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
--- Proje_OOP/Controllers/ProductController.cs
+++ Proje_OOP/Controllers/ProductController.cs
@@ -25,5 +25,33 @@ namespace Proje_OOP.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = context.Products.Where(x=> x.Id == id).FirstOrDefault();
+            context.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = context.Products.Where(x => x.Id == id).FirstOrDefault();
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            var value = context.Products.Where(x => x.Id == product.Id).FirstOrDefault();
+            value.Name = product.Name;
+            value.Price = product.Price;
+            value.Stock = product.Stock;
+            context.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
